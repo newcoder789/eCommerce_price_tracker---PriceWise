072df6bd1c1347d9b78cc0feab6ec7b9198bc7af
newcoder789 <mymail@gmail.com>
2024-11-24 17:08:23 +0530

    Navbar Completed

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,6 +1,13 @@
 import Link from 'next/link'
 import React from 'react'
 import Image from 'next/image'
+
+
+const navIcons = [
+  {src:'/assets/icons/search.svg',alt:'search'},
+  {src:'/assets/icons/black-heart.svg',alt:'black-heart'},
+  {src:'/assets/icons/user.svg',alt:'user'},
+]
 const Navbar = () => {
   return (
     <header className='w-full'>
@@ -10,8 +17,13 @@ const Navbar = () => {
               <p className='nav-logo'>
                   Price<span className='text-primary'>Wise</span>
               </p>
-
         </Link>
+        <div className='flex items-center gap-5'>
+          {navIcons.map((icon)=>{
+            return <Image key={icon.alt} src={icon.src} alt={icon.alt} width={28} height={28} className='object-contain'/>
+          })}
+
+        </div>
 
       </nav>
     </header>
